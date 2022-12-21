0fe7d70d4a7fbf4eb251008f370b7e45e85f38c3
jeno <Jenodenslagen@hotmail.com>
2022-12-21 10:14:38 +0100

    Changes
    update

--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -9,7 +9,7 @@ public class PlayerCombat : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (Input.GetKeyDown(KeyCode.LeftShift))
         {
             Attack();
         }
--- Assets/Scripts/PlayerMovements.cs
+++ Assets/Scripts/PlayerMovements.cs
@@ -7,14 +7,19 @@ public class PlayerMovements : MonoBehaviour
     private Rigidbody2D rb;
     [SerializeField] float movespeed = 6;
     private SpriteRenderer sprite;
+    private BoxCollider2D coll;
+
+    [SerializeField] private LayerMask jumpableGround;
 
     private Animator anim;
     private float dirX;
     [SerializeField]private float jumpforce = 14f;
+    private enum MovementState {idle, running, attack, crit}
     // Start is called before the first frame update
    private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
         sprite = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
     }
@@ -25,7 +30,7 @@ public class PlayerMovements : MonoBehaviour
 
         dirX = Input.GetAxisRaw("Horizontal");
             rb.velocity = new Vector2(dirX * movespeed, rb.velocity.y);
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpforce);
         }
@@ -48,5 +53,10 @@ public class PlayerMovements : MonoBehaviour
             anim.SetBool("running", false);
             sprite.flipX = false;
         }
+       
+    }
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
 }
