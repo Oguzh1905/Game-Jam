71e12dd6192bb3a854bf2ec41cbf7929c5960351
Oguzhan <2178095@talnet.nl>
2022-12-22 09:05:30 +0100

    Update

--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -6,42 +6,24 @@ public class PlayerCombat : MonoBehaviour
 {
 
     public Animator animator;
-    
+
     public Transform attackpoint;
     public float attackRange = 0.5f;
     public LayerMask enemyLayers;
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< main
-    public float attackRate = 4f;
-=======
 
-    public float attackRate = 3f;
+    public float attackRate = 5f;
 
->>>>>>> main
     float nextAttackTime = 0f;
     public int attackDamage = 40;
     // Update is called once per frame
     void Update()
     {
-        if(Time.time >= nextAttackTime)
-=======
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Q))
->>>>>>> Knight
-=======
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Q))
->>>>>>> Stephan
+        if (Time.time >= nextAttackTime)
         {
             if (Input.GetKeyDown(KeyCode.Q))
             {
                 Attack();
-                nextAttackTime = Time.time + 1f / attackRate;
+                nextAttackTime = Time.time + 2f / attackRate;
             }
         }
     }
@@ -52,17 +34,9 @@ public class PlayerCombat : MonoBehaviour
         //enemies in range
         Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackpoint.position, attackRange, enemyLayers);
         //damage them
-        foreach(Collider2D enemy in hitEnemies)
+        foreach (Collider2D enemy in hitEnemies)
         {
-<<<<<<< HEAD
-<<<<<<< main
             enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
-=======
-            Debug.Log("We hit " + enemy.name);
->>>>>>> Knight
-=======
-            Debug.Log("We hit " + enemy.name);
->>>>>>> Stephan
         }
     }
     private void OnDrawGizmosSelected()
