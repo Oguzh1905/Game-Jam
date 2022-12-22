d3e639d0369e15389c614d74a95c9cf26879af7d
Rick <2178099@talnet.nl>
2022-12-22 10:46:38 +0100

    dead UI and Victory UI

---
+++ Assets/UI you died screen/DeadScreen.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeadScreen : MonoBehaviour
+{
+    public float waitforAnyKey = 2f;
+
+    public GameObject anyKeyTest;
+
+    public string mainMenuScene;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (waitforAnyKey > 0)
+        {
+            waitforAnyKey -= Time.deltaTime;
+            if (waitforAnyKey <= 0)
+            {
+                anyKeyTest.SetActive(true);
+            }
+        }
+        else
+        {
+            if (Input.anyKeyDown)
+            {
+                SceneManager.LoadScene(mainMenuScene);
+            }
+        }
+
+
+    }
+}
